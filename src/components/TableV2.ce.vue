0e102f73a83d68fbdd1717a896e8a16edcd2b3ba
Chris <chrisbreuer93@gmail.com>
2022-04-20 12:28:54 -0700

    chore: wip

--- src/components/TableV2.ce.vue
+++ src/components/TableV2.ce.vue
@@ -1,38 +1,131 @@
 <script setup lang="ts">
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
+
 interface Props {
-  title: string
+  host: string
 }
 
 const props = defineProps<Props>()
 
 // reactive state
-let count = $ref(0)
+const searchClient = $ref(instantMeiliSearch(props.host, ''))
+let sort = $ref('')
+const isSorted = $computed(() => sort !== '')
 
-// functions that mutate state and trigger updates
-function increment() {
-  count++
+function toggleSort(order: string) {
+  if (isSorted)
+    sort = ''
+  else
+    sort = order
 }
 
 // lifecycle hooks
 onMounted(() => {
   // eslint-disable-next-line no-console
-  console.log(`The initial count is ${count}.`)
-  // eslint-disable-next-line no-console
-  console.log(`The title is ${props.title}.`)
+  console.log('sc', searchClient)
 })
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold text-red-800">
-    {{ props.title }}
-  </h1>
-  <button
-    type="button"
-    class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-    @click="increment"
-  >
-    Count is: {{ count }}
-  </button>
+  <div class="table-v2 px-4 sm:px-6 lg:px-8 font-sans">
+    <div class="sm:flex sm:items-center">
+      <div class="sm:flex-auto">
+        <h1 class="text-xl font-semibold text-gray-900">
+          Users
+        </h1>
+        <p class="mt-2 text-sm text-gray-700">
+          A list of all the users in your account including their name, title, email and role.
+        </p>
+      </div>
+      <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+        <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+          Add user
+        </button>
+      </div>
+    </div>
+    <div class="mt-8 flex flex-col">
+      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+          <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" @click="toggleSort('name')">
+                    <a href="#" class="group inline-flex">
+                      Name
+                      <span :class="sort === 'name' ? `invisible text-gray-400 group-hover:visible group-focus:visible` : `bg-gray-200 text-gray-900 group-hover:bg-gray-300`" class="ml-2 flex-none rounded">
+                        <!-- Heroicon name: solid/chevron-down -->
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" @click="toggleSort('title')">
+                    <a href="#" class="group inline-flex">
+                      Title
+                      <span :class="sort === 'title' ? `invisible text-gray-400 group-hover:visible group-focus:visible` : `bg-gray-200 text-gray-900 group-hover:bg-gray-300`" class="ml-2 flex-none rounded">
+                        <!-- Heroicon name: solid/chevron-down -->
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" @click="toggleSort('email')">
+                    <a href="#" class="group inline-flex">
+                      Email
+                      <span :class="sort === 'email' ? `invisible text-gray-400 group-hover:visible group-focus:visible` : `bg-gray-200 text-gray-900 group-hover:bg-gray-300`" class="ml-2 flex-none rounded">
+                        <!-- Heroicon name: solid/chevron-down -->
+                        <svg class="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900" @click="toggleSort('role')">
+                    <a href="#" class="group inline-flex">
+                      Role
+                      <span :class="sort === 'role' ? `invisible text-gray-400 group-hover:visible group-focus:visible` : `bg-gray-200 text-gray-900 group-hover:bg-gray-300`" class="ml-2 flex-none rounded">
+                        <!-- Heroicon name: solid/chevron-down -->
+                        <svg class="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200 bg-white">
+                <tr>
+                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                    Lindsay Walton
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    Front-end Developer
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    lindsay.walton@example.com
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    Member
+                  </td>
+                  <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                  </td>
+                </tr>
+
+              <!-- More people... -->
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
