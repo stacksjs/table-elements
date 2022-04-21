5721f538ea94d7654390fb3eedf0c7cf9ade2dae
Chris <chrisbreuer93@gmail.com>
2022-04-21 00:37:21 -0700

    chore: wip

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { defineCustomElement } from 'vue'
 import Table from './components/TableV2.ce.vue'
 import TableConfigure from './components/TableConfigure.ce.vue'
 
-// console.log('Table.styles', Table.styles) // ["/* inlined css */"]
-
 // convert into custom element constructor
 const TableCustomElement = defineCustomElement(Table)
 const TableConfigureCustomElement = defineCustomElement(TableConfigure)
 
 // export individual elements
-// export { TableCustomElement, AnotherCustomElement };
+// export { TableCustomElement, TableConfigureCustomElement };
 
 // export function register() {
 customElements.define('table-v2', TableCustomElement)
